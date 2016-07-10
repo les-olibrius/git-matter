ee6fba7691376ae21aaa7c8cdbe14dd4ec8be574
Maxime Baumann <maxime.baumann.pro@gmail.com>
2016-07-10 14:46:51 +0200

    Deport code

--- public/index.php
+++ public/index.php
@@ -4,35 +4,6 @@ namespace LesOlibrius\GitMatter;
 
 require '../vendor/autoload.php';
 
-// CONST
-
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_PATH', dirname(__DIR__));
-
-define('CONTENT_PATH', ROOT_PATH . DS . 'content');
-define('CONFIG_PATH', ROOT_PATH . DS . 'config');
-define('LOCALES_PATH', ROOT_PATH . DS . 'locales');
-define('VIEWS_PATH', ROOT_PATH . DS . 'views');
-
 $app = new GitMatterApp();
-
-# region Providers
-
-$app->register(new \Silex\Provider\LocaleServiceProvider());
-$app->register(new \Silex\Provider\TranslationServiceProvider(), array(
-    'locale_fallbacks' => array('en'),
-));
-
-$app->register(new \Silex\Provider\ValidatorServiceProvider());
-
-$app->register(new \Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => VIEWS_PATH,
-));
-
-# endregion Providers
-
-$app->get('/{_locale}/{wikiPage}', function ($wikiPage) use ($app){
-
-});
 
 $app->run();
--- src/GitMatterApp.php
+++ src/GitMatterApp.php
@@ -3,9 +3,10 @@
 namespace LesOlibrius\GitMatter;
 
 use Silex\Application;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 /**
- * Class App
+ * Class GitMatterApp
  * @author Maxime Baumann <maxime.baumann.pro@gmail.com>
  * @package LesOlibrius\GitMatter
  */
@@ -14,5 +15,114 @@ class GitMatterApp extends \Silex\Application
     use Application\TwigTrait;
     use Application\SecurityTrait;
     use Application\TranslationTrait;
+
+    /**
+     * @var string
+     */
+    private $rootPath;
+
+    /**
+     * @var string
+     */
+    private $configDirPath;
+
+    /**
+     * @var string
+     */
+    private $localesDirPath;
+
+    /**
+     * @var string
+     */
+    private $viewsDirPath;
+
+    /**
+     * @var string
+     */
+    private $contentDirPath;
+
+    public function __construct(array $values = array())
+    {
+        parent::__construct($values);
+
+        $this->setPaths();
+
+        $this->registerProviders();
+        $this->setRoutes();
+    }
+
+    private function setPaths()
+    {
+        $this->rootPath = dirname(__DIR__);
+        $this->configDirPath = $this->rootPath.DIRECTORY_SEPARATOR.'config';
+        $this->localesDirPath = $this->rootPath.DIRECTORY_SEPARATOR.'locales';
+        $this->viewsDirPath = $this->rootPath.DIRECTORY_SEPARATOR.'views';
+        $this->contentDirPath = $this->rootPath.DIRECTORY_SEPARATOR.'content';
+    }
+
+    private function registerProviders()
+    {
+        $self = $this;
+
+        // Session
+        $this->register(new \Silex\Provider\SessionServiceProvider());
+
+        // Locale and Translation
+        #region Locale and translattion
+        $this->register(new \Silex\Provider\LocaleServiceProvider());
+        $this->register(
+            new \Silex\Provider\TranslationServiceProvider(),
+            array(
+                'locale_fallbacks' => array('en'),
+            )
+        );
+
+        $this->extend(
+            'translator',
+            function ($translator, $self) use ($self) {
+                $translator->addLoader('yaml', new YamlFileLoader());
+
+                $localesPathFormat = $self->localesDirPath.DIRECTORY_SEPARATOR.'*.yml';
+                foreach (glob($localesPathFormat) as $file) {
+                    $locale = str_replace([$self->localesDirPath.DIRECTORY_SEPARATOR, '.yml'], '', $file);
+
+                    $translator->addResource('yaml', $file, $locale);
+                }
+
+                return $translator;
+            }
+        );
+        #endregion
+
+        $this->register(new \Silex\Provider\ValidatorServiceProvider());
+
+        $this->register(
+            new \Silex\Provider\TwigServiceProvider(),
+            array(
+                'twig.path' => $this->viewsDirPath,
+            )
+        );
+    }
+
+    private function setRoutes()
+    {
+        $self = $this;
+        $this->get(
+            '/',
+            function () use ($self) {
+                $url = '/'.$self['locale'];
+
+                return $this->redirect($url);
+            }
+        );
+
+        $this->get(
+            '/{_locale}/{wikiPage}',
+            function ($wikiPage) use ($self) {
+                return $wikiPage;
+            }
+        )
+            ->value('wikiPage', 'index');
+    }
 
 }
